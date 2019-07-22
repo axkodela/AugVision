4fe84f773288fd2706493f05a329bd9015896924
axkodela <52983607+axkodela@users.noreply.github.com>
2019-07-21 21:24:28 -0500

    Add files via upload

---
+++ PGMStuff/PGMfiletest.py
@@ -0,0 +1,39 @@
+##PGM Test1
+
+import random
+
+## Creating Each Singular Array inside of the outer one.
+def mkarray():
+	x = [] ## singular array with numbers
+	for i in range(300): 
+		x.append(random.randint(0,255)) ## append random numbers as each element 
+	return x
+## end
+
+bigy = [] ## outer array.
+
+for n in range(300):
+	bigy.append(mkarray())
+
+
+## Printing to file stuff ##
+def ArrToString(arr):
+	tmp1 = ""
+	for i in arr:
+		tmp1 += str(i) + " "
+	return tmp1
+
+zerosArray = []
+for i in range(24):
+	zerosArray.append(int("0"))
+
+def toFile(arr):
+	myf = open("test3.pgm","w")
+	myf.write("P5\n300 300\n255\n")
+	print(zerosArray)
+	for i in arr:
+		myf.write(ArrToString(i))
+		print(i)
+	myf.close()
+
+toFile(bigy)
